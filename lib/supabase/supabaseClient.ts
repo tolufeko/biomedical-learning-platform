383765ba9cac2600b097a5932badb7d156b25fd8
olufekot <olufekot@yahoo.com>
2026-03-29 15:31:00 +0100

    remove legacy software like h5p

--- lib/supabase/supabaseClient.ts
+++ lib/supabase/supabaseClient.ts
@@ -1,7 +1,7 @@
 // lib/supabaseClient.ts
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createBrowserClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
